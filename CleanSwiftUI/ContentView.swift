2c823500b67b0d9a3c4f80f4cff3ee6417763df9
Vitor Moriya <vitor.moriya@profusion.mobi>
2023-01-16 13:01:50 -0300

    feat(ContentView): add list

--- CleanSwiftUI/ContentView.swift
+++ CleanSwiftUI/ContentView.swift
@@ -1,21 +1,48 @@
-//
-//  ContentView.swift
-//  CleanSwiftUI
-//
-//  Created by Vitor Moriya on 16/01/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Image(systemName: "person")
+                        .foregroundColor(.black)
+
+                    VStack(alignment: .leading) {
+                        Text("Name Surname")
+                        Text("Person A")
+                            .foregroundColor(.gray)
+                            .font(.footnote)
+                    }
+                    .padding(.leading, 10)
+                }
+
+                Divider()
+
+                HStack {
+                    Image(systemName: "person.circle")
+                        .foregroundColor(.blue)
+
+                    Text("This is item number one")
+                        .foregroundColor(.blue)
+                }
+                .padding(.vertical, 10)
+
+                Divider()
+
+                HStack {
+                    Image(systemName: "person")
+                        .foregroundColor(.red)
+
+                    Text("This is item number two")
+                        .foregroundColor(.red)
+                }
+                .padding(.top, 10)
+
+                Divider()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
