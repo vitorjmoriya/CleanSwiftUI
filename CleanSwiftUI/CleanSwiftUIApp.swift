34a6a28283095536d44b068c3fad98fd4aedfcd4
Vitor Moriya <vitor.moriya@profusion.mobi>
2023-01-16 11:03:10 -0300

    chore: initial commit

---
+++ CleanSwiftUI/CleanSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSwiftUIApp.swift
+//  CleanSwiftUI
+//
+//  Created by Vitor Moriya on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CleanSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
